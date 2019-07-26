6001b2d5d24d01f66d8cd2b7b48032757df85f20
SionchezCar <csionchez@acmetic.com.pe>
2019-07-26 10:23:37 -0500

    CrudArchivosAjaxBlade
    Vista para el nuevo Crud.

--- resources/views/CRUDArchivosAJAX/CrudArchivosAjax.blade.php
+++ resources/views/CRUDArchivosAJAX/CrudArchivosAjax.blade.php
@@ -16,30 +16,43 @@
 		<title>CRUD Archivos Ajax</title>
 	</head>
 	<body>
-		<div class="row container">
+
+	{{-- Menu - { --}}
+{{--   		<nav>
+  		  <div class="nav-wrapper">
+  		    <a href="#!" class="brand-logo center">Logo</a>
+  		    <ul class="left hide-on-med-and-down">
+  		      <li><a href="sass.html">Sass</a></li>
+  		      <li><a href="badges.html">Components</a></li>
+  		      <li class="active"><a href="collapsible.html">JavaScript</a></li>
+  		    </ul>
+  		  </div>
+  		</nav> --}}
+  	{{-- Menu - { --}}
+
+	{{-- Formulario - { --}}
+	 	<div class="row container">
 			<div class="col s12">
 				<h4>Mis Visitas:</h4>
 			</div>
 		</div>
 
   		<div class="container">
-  		  {{-- <form id="formulario" method="GET" enctype="multipart/form-data"> --}}
-			{{-- @csrf --}}
 
   		    <div class="row">
   		      <div class="input-field col s6">
-          		<input id="nombre" type="text">
+          		<input id="nombre" name="nombre" type="text">
           		<label for="nombre">Nombre</label>
   		      </div>
   		      <div class="input-field col s6">
-          		<input id="ubicacion" type="text">
+          		<input id="ubicacion" name="ubicacion" type="text">
           		<label for="ubicacion">Ubicación</label>
   		      </div>
   		    </div>
 
   		    <div class="row">
     			<div class="input-field col s12">
-    			     <textarea id="nota" class="materialize-textarea"></textarea>
+    			     <textarea id="nota" name="nota" class="materialize-textarea"></textarea>
     			     <label for="nota">Nota</label>
     			</div>
   		    </div>
@@ -49,7 +62,7 @@
     			<div class="file-field input-field">
     			  <div class="btn">
     			    <span>Foto</span>
-    			    <input type="file">
+    			    <input id="foto" name="foto[]" type="file">
     			  </div>
     			  <div class="file-path-wrapper">
     			    <input class="file-path" type="text">
@@ -58,61 +71,61 @@
   		      </div>
   		    </div>
 
-  		  {{-- </form> --}}
-
-  		    <button class="btn waves-effect waves-light" type="submit" name="action" onclick="enviar()">Enviar
-    			<i class="material-icons right">send</i>
-  			</button>
-
-  		    <div class="row margin-default">
-    			<div class="col s4">
-    			  <div class="card">
-    			    <div class="card-image">
-    			      <img src="#">
-    			      <span class="card-title">Nombre(s) y Apellido(s)</span>
-    			    </div>
-    			    <div class="card-content">
-    			      <p>Texto de prueba.</p>
-    			    </div>
-    			    <div class="card-action">
-    			      <a href="#">Más</a>
-    			    </div>
-    			  </div>
-    			</div>
-
-    			<div class="col s4">
-    			  <div class="card">
-    			    <div class="card-image">
-    			      <img src="#">
-    			      <span class="card-title">Nombre(s) y Apellido(s)</span>
-    			    </div>
-    			    <div class="card-content">
-    			      <p>Texto de prueba.</p>
-    			    </div>
-    			    <div class="card-action">
-    			      <a href="#">Más</a>
-    			    </div>
-    			  </div>
-    			</div>
-
-    			<div class="col s4">
-    			  <div class="card">
-    			    <div class="card-image">
-    			      <img src="#">
-    			      <span class="card-title">Nombre(s) y Apellido(s)</span>
-    			    </div>
-    			    <div class="card-content">
-    			      <p>Texto de prueba.</p>
-    			    </div>
-    			    <div class="card-action">
-    			      <a href="#">Más</a>
-    			    </div>
-    			  </div>
-    			</div>
-
-  			</div>
-
+			<button class="btn waves-effect waves-light" id="guardar" name="guardar" type="submit" onclick="guardar()">Guardar</button>
   		</div>
+  	{{-- Formulario - } --}}
+
+  	{{-- Visitas - { --}}
+  		<div class="container">
+  			<div class="row margin-default">
+  				@foreach($Visitas as $Vis)
+    				<div class="col s4">
+    			  		<div class="card">
+    			    		<div class="card-image">
+    			      			<img src="{{$Vis->foto}}">
+    			      			<span class="card-title">{{$Vis->nombre}}</span>
+    			    		</div>
+    			    		<div class="card-content">
+    			      			<p><b>Nota de Viaje:</b> {{$Vis->nota}}</p>
+    			    		</div>
+    			    	<div class="card-action">
+    			    		<p><b>Ubicacion:</b> {{$Vis->ubicacion}}</p>
+    			      		<p><b>Fecha:</b> {{date('d/m/Y', strtotime($Vis->created_at))}}</p>
+    			    	</div>
+    			  	</div>
+    				</div>
+    			@endforeach
+  			</div>
+  		</div>
+	{{-- Visitas - } --}}
+
+	{{-- Pie de pagina - { --}}
+{{--   		<footer class="page-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Footer Content</h5>
+                <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Links</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2014 Copyright Text
+            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+          </div>
+        </footer> --}}
+    {{-- Pie de pagina - } --}}
 
 	</body>
 </html>
@@ -121,11 +134,30 @@
 <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 
 <script>
-	function enviar(){
+	function guardar(){
 		var nombre = $('#nombre').val();
 		var ubicacion = $('#ubicacion').val();
 		var nota = $('#nota').val();
 
-		alert(apellidos+' '+nombres+' '+nota);
+		$.ajax({
+			url: '/CRUDArchivosAJAX/CrudArchivosAjax/store',
+			type: 'GET',
+			data: {
+				nombre: nombre, 
+				ubicacion: ubicacion, 
+				nota: nota,
+			},
+		})
+		.done(function(data) {
+
+			console.log(data.mensaje);
+
+		})
+		.fail(function() {
+			console.log("error");
+		})
+		.always(function() {
+			console.log("complete");
+		});
 	}
 </script>
